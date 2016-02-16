f5f3535fc8210a2698201c587ea642fb1ff3db84
Garima Sharma <garimasharma2309@gmail.com>
2016-02-16 16:44:14 -0500

    Added file for Virtual Tour of UC

---
+++ VTApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VTApp.Startup))]
+namespace VTApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
